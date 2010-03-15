c494c0f3552489d7cf9e6d1be5a831c670f27815
Terence Lee <hone02@gmail.com>
2010-03-15 15:53:09 -0500

    execute the right bundler command

---
+++ lib/webrat/selenium/application_servers/bundler_rails.rb
@@ -0,0 +1,17 @@
+module Webrat
+  module Selenium
+    module ApplicationServers
+      class BundlerRails < Rails
+
+        def start_command
+        "bundle exec mongrel_rails start -d --chdir='#{RAILS_ROOT}' --port=#{Webrat.configuration.application_port} --environment=#{Webrat.configuration.application_environment} --pid #{pid_file} &"
+        end
+
+        def stop_command
+        "bundle exec mongrel_rails stop -c #{RAILS_ROOT} --pid #{pid_file}"
+        end
+
+      end
+    end
+  end
+end
